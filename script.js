c86cfdfe25a670b7e3cb1732f2981a28a9db21a4
Slinevision <wandilesawukelo37@gmail.com>
2026-07-31 23:04:59 +0200

    Add searchProducts function to filter products

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function addToCart() {
   cart++;
   document.getElementById("cart-count").innerText = cart;
 }
+
+function searchProducts() {
+  let input = document.getElementById("search").value.toLowerCase();
+  let products = document.getElementsByClassName("product");
+
+  for (let i = 0; i < products.length; i++) {
+    let title = products[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+
+    if (title.includes(input)) {
+      products[i].style.display = "block";
+    } else {
+      products[i].style.display = "none";
+    }
+  }
+}
